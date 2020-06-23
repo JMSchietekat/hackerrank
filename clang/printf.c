381d291b265b5d00758d41b56e44293574d78d1c
JMSchietekat <justinschietekat@gmail.com>
2020-06-23 09:29:15 +0200

    Demo printf in clang.

---
+++ clang/printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{	
+    printf("Integer: %d, character: %c, string %s and float %f\n", 5, 'c', "Hello", 7.3);
+    return 0;
+}
