f6abd83aa103d93b6de4646fcce166ca186b9c2a
JMSchietekat <justinschietekat@gmail.com>
2020-06-23 23:31:40 +0200

    Added functions solution in c.

---
+++ clang/functions.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d)
+{
+	int max_int = a;
+
+	if(b >= max_int) max_int = b;
+	if(c >= max_int) max_int = c;
+	if(d >= max_int) max_int = d;
+	
+	return max_int;
+}
+
+int main()
+{
+	int a, b, c, d;
+	scanf("%d %d %d %d", &a, &b, &c, &d);
+	int ans = max_of_four(a, b, c, d);
+	printf("%d", ans);
+
+	return 0;
+}
