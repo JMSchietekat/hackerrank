af842c219ca47fe21257e958afbd902128cefaeb
JMSchietekat <justinschietekat@gmail.com>
2020-07-01 14:04:27 +0200

    Complete linked list challange.

---
+++ 30-day-of-code/day-15-linked-list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Solution:
+    def display(self, head):
+        current = head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+
+    def insert(self, head, data):
+        if(head == None): 
+            return Node(data)
+            
+        current = head
+
+        while current:
+            if(current.next == None):
+                current.next = Node(data)
+                return head
+
+            current = current.next
+
+
+mylist = Solution()
+T = int(input())
+head = None
+for i in range(T):
+    data = int(input())
+    head = mylist.insert(head, data)
+mylist.display(head)
