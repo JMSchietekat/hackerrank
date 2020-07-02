a803ab4e596e3aa6edcf3d2bf86c5882890f7cd8
JMSchietekat <justinschietekat@gmail.com>
2020-07-02 21:34:37 +0200

    Complete interquartile range challange.

---
+++ 10-days-of-statistics/day-1-interquartile-range.py
@@ -0,0 +1,46 @@
+def interquartileRange(x, f):
+    s = []
+
+    for i, v in enumerate(x):
+        for _ in range(f[i]):
+            s.append(v)
+
+    q = quartiles(s)
+
+    return (q[2] - q[0])
+
+
+def median(x=[]):
+    x.sort()
+    x_cnt = len(x)
+
+    if(x_cnt % 2 == 0):
+        return (x[int(x_cnt/2) - 1] + x[int(x_cnt/2)])/2
+    else:
+        return x[int(x_cnt/2)]
+
+
+def quartiles(x=[]):
+    x.sort()
+    q2 = median(x)
+
+    setCnt = len(x)
+
+    if(setCnt % 2 == 0):
+        q1 = median(x[:int(setCnt/2)])
+        q3 = median(x[int(setCnt/2):])
+    else:
+        q1 = median(x[:int(setCnt/2)])
+        q3 = median(x[int(setCnt/2)+1:])
+
+    return q1, q2, q3
+
+
+if __name__ == "__main__":
+
+    n = int(input())
+
+    x = list(map(int, input().rstrip().split()))
+    f = list(map(int, input().rstrip().split()))
+
+    print(round(interquartileRange(x, f) * 1.0, 1))
