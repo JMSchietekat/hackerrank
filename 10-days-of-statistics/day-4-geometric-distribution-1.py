00817bcf40a345d0cc6d0ff6753ac104ba031e5a
JMSchietekat <justinschietekat@gmail.com>
2020-07-05 22:39:15 +0200

    Submit geometric distribution 1 challange.

---
+++ 10-days-of-statistics/day-4-geometric-distribution-1.py
@@ -0,0 +1,29 @@
+def factorial(n):
+    return 1 if n == 0 else n * factorial(n - 1)
+
+
+def combination(n, x):
+    return factorial(n)/(factorial(x) * factorial(n-x))
+
+
+def negativeBinomialProbability(x, n, p):
+    # x = number of successes
+    # n = total number of trials
+    # The probability of success of 1 trial is p.
+    # The probability of failure of 1 trial q , where q = 1 - p.
+
+    return combination(n - 1, x - 1) * pow(p, x) * pow((1-p), (n-x))
+
+
+def geometricProbability(n, p):
+    return negativeBinomialProbability(1, n, p)
+
+
+if __name__ == "__main__":
+
+    numerator, denominator = map(int, input().split())
+    n = int(input())
+
+    p = numerator / denominator
+
+    print('{:.3f}'.format(geometricProbability(n, p)))
