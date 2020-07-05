f7b1d264e8e1da6332ef4c480c997f77b23175ba
JMSchietekat <justinschietekat@gmail.com>
2020-07-05 22:10:42 +0200

    Simplify recursive factorial function.

--- 30-day-of-code/day-9-recursion-3.py
+++ 30-day-of-code/day-9-recursion-3.py
@@ -8,9 +8,7 @@ import sys
 
 # Complete the factorial function below.
 def factorial(n):
-	if (n == 1): return 1
-
-	return n * factorial(n-1)
+	return 1 if n == 0 else n * factorial(n - 1)
 
 
 if __name__ == '__main__':
