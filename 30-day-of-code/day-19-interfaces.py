4180a506eaf938b24cda4a926b6c5a86f30b720f
JMSchietekat <justinschietekat@gmail.com>
2020-07-05 11:02:52 +0200

    Submit interfaces challange.

---
+++ 30-day-of-code/day-19-interfaces.py
@@ -0,0 +1,19 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        divisorSum = 0
+
+        for x in range(1,n):
+            if(n % x == 0):
+                divisorSum += x
+
+        return divisorSum + n
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
