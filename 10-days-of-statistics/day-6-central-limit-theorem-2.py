900e94cfd47abfeb91d3a6cf89b90d347302455a
JMSchietekat <justinschietekat@gmail.com>
2020-07-07 21:21:08 +0200

    Submit central limit theory 3 challange.

--- 10-days-of-statistics/day-6-central-limit-theorem-2.py
+++ 10-days-of-statistics/day-6-central-limit-theorem-2.py
@@ -1,19 +1,12 @@
 import math
 
-
-def norm_cdf(x, mean, stddev):
-    return 0.5 * (1 + math.erf((x - mean) / (stddev * (2 ** 0.5))))
-
-
 if __name__ == "__main__":
 
-    x = int(input())
     n = int(input())
-    mu = float(input())
-    phi = float(input())
+    mu = int(input())
+    sd = float(input())
+    interval = float(input())
+    z_score = float(input())
 
-    mu_prime = n * mu
-    phi_prime = math.sqrt(n) * phi
-
-    # Probability that 100 students can successfully purchase the remaining 250 tickets
-    print('{:.4f}'.format(norm_cdf(x, mu_prime, phi_prime)))
+    print('{:.2f}'.format(mu - (sd / math.sqrt(n)) * z_score))
+    print('{:.2f}'.format(mu + (sd / math.sqrt(n)) * z_score))
