408ab7d90c883be4234cdbd3d093dd77c4368fda
JMSchietekat <justinschietekat@gmail.com>
2020-07-10 22:57:38 +0200

    Submit staircase solution.

---
+++ problem-solving/staircase.py
@@ -0,0 +1,19 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+def staircase(n):
+    line = '#'
+    
+    for _ in range(n):
+        print(line.rjust(n))
+        line += '#'
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
