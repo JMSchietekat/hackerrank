c5b754d133957bf9ffe140a6fa3c0e32553e9a3b
JMSchietekat <justinschietekat@gmail.com>
2020-07-16 19:00:19 +0200

    Submit time convertion challange.

---
+++ problem-solving/time-convertion.py
@@ -0,0 +1,12 @@
+import time
+
+def timeConversion(s):
+    time_obj = time.strptime(s, "%I:%M:%S%p")
+    return time.strftime("%H:%M:%S", time_obj)
+
+if __name__ == '__main__':
+    s = input()
+
+    print(timeConversion(s))
+
+    
