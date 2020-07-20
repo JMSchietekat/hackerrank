d2cb99055230742947354b89868f575f02e6981c
JMSchietekat <justinschietekat@gmail.com>
2020-07-20 20:25:49 +0200

    Submit breaking the records challange.

---
+++ problem-solving/breaking-the-records.py
@@ -0,0 +1,34 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+def breakingRecords(scores):
+    high = scores[0]
+    low = scores[0]
+
+    count_h_l = [0,0]
+
+    for s in scores:
+        if s > high:
+            high = s
+            count_h_l[0] += 1
+
+        if s < low:
+            low = s
+            count_h_l[1] += 1
+
+    return count_h_l
+
+
+if __name__ == '__main__':
+
+    n = int(input())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    print(result[0], result[1])
