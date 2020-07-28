8a8a1723cc42f4e70f74fb1cb9ce2edc512f4b95
JMSchietekat <justinschietekat@gmail.com>
2020-07-28 22:12:55 +0200

    Submit drawing book challange.

---
+++ problem-solving/drawing-book.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import os
+import sys
+import math
+
+def pageCount(n, p):
+    pageTurns = math.floor(p/2)
+    totalTurns = math.floor(n/2)
+
+    return min(pageTurns, totalTurns - pageTurns)
+
+if __name__ == '__main__':
+
+    n = int(input())
+
+    p = int(input())
+
+    print(pageCount(n, p))
+
+    
